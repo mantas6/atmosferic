33151258da451b69524ff31b6f3c284153f0db9c
Mantas <pauliukonis.mantas@gmail.com>
2023-07-07 09:32:30 +0300

    Update doorbell.js

--- js/doorbell.js
+++ js/doorbell.js
@@ -30,34 +30,13 @@ $(function()
     {
         location.replace(branches[$(this).val()]);
     });
-
-    a_sites_auto_collection('Atmosferic');
 });
 
 var Doorbell = {
 
     call: function(name, info)
     {
-        var nameOfItem = info.info;
         
-        var params = {
-            name: name,
-            titles: { name: nameOfItem },
-            attachments: {},
-            values: {
-                cost: { value: info.cost },
-                money: { value: Math.round(client.session.money) },
-                distance: { value: Math.round(client.session.distance) },
-            },
-        };
-
-        if(name == 'error')
-        {
-            params.attachments.save = JSON.stringify(client.session);
-            params.attachments.trace = JSON.stringify(info);
-        }
-
-        a_sites_request('Atmosferic', params);
     },
 
 };
